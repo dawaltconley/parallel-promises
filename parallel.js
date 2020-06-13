ca9e3e24c23024225a7a068948bc3bae3fc9a987
Dylan Awalt-Conley <web@dylan.ac>
2020-06-12 20:02:19 -0400

    added semicolons

--- parallel.js
+++ parallel.js
@@ -1,19 +1,19 @@
 module.exports = async (tasks, n) => {
-    results = []
-    executing = []
+    results = [];
+    executing = [];
     while (tasks.length) {
         if (executing.length < n) {
-            let task = tasks.shift()
+            let task = tasks.shift();
             task = task().then(r => {
-                const i = executing.indexOf(task)
-                executing.splice(i, 1)
-                return r
+                const i = executing.indexOf(task);
+                executing.splice(i, 1);
+                return r;
             })
-            executing.push(task)
+            executing.push(task);
         } else {
-            const next = await Promise.race(executing)
-            results.push(next)
+            const next = await Promise.race(executing);
+            results.push(next);
         }
     }
-    return results
+    return results;
 }
